3f75c72c6726933d5fc61e3c02278a9fa49603ce
Ryan Bender <ryanbender65@yahoo.com>
2020-10-05 22:14:39 -0400

    bot: added logging and other updates

--- bot.py
+++ bot.py
@@ -1,12 +1,21 @@
 from datetime import datetime, timedelta
 from youtube_api import YouTubeDataAPI
-from instabot import Bot
+from pytube import YouTube
 from csv import reader
 from threading import Thread
 from time import sleep
+from random import randint
+import ssl
+from smtplib import SMTP_SSL
 import logging
 
-YOUTUBE = YouTubeDataAPI(open("C:\\MSI\\bot_key.key", 'r').readline())
+EMAIL_PASS = open("C:\\MSI\\email_pass.key", 'r').readline()
+EMAIL_SERVER = SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context())
+EMAIL_SERVER.login('ak.insta.contact@gmail.com', EMAIL_PASS)
+
+def email(subject: str, body: str) -> None:
+    message = f'Subject: {subject}\n\n{body}'
+    EMAIL_SERVER.sendmail('ak.insta.contact@gmail.com', 'ryan.bender.general@gmail.com', message)
 
 
 class VideoHandler(Thread):
@@ -15,26 +24,39 @@ class VideoHandler(Thread):
 
     """
     _video_id: str
+    _video_path: str
 
     def __init__(self, video_id: str) -> None:
+        super().__init__()
+
         self._video_id = video_id
 
-    def run(self) -> None:
-        one_day_ago = datetime.today() - timedelta(days=5)
-        video_search = [i['video_id'] for i in YOUTUBE.search(channel_id='UCyhEZKz-LOwgktptEOh6_Iw', published_after=one_day_ago, order_by='date')]
-        uploaded_videos = [i[0] for i in reader(open('uploaded_videos.csv', 'r'))]
+        date = datetime.today().strftime('%m-%d-%Y')
+        filename = f'temp{randint(10000, 99999)}_{date}'
+        self._download_video(filename)
 
-        with open('uploaded_videos.csv', 'a') as file:
-            for video in video_search:
-                if video not in uploaded_videos:
-                    file.write(video + '\n')
+    def _download_video(self, new_filename: str) -> None:
+        yt = YouTube('https://www.youtube.com/watch?v=' + self._video_id)
+        filepath = yt.streams.first().download('C:\\Users\\ryanb\\OneDrive\\Documents\\' +
+                'GitHub\\Andrew-Klavan-Instragram-Bot\\temp_video_storage\\', new_filename)
+        self._video_path = filepath
+
+    def run(self) -> None:
+        # with open('uploaded_videos.csv', 'a') as file:
+        #     for video in video_search:
+        #         if video not in uploaded_videos:
+        #             file.write(video + '\n')
+        print(self._video_id)
+        print(self._video_path)
 
 
 def main():
+    youtube = YouTubeDataAPI(open("C:\\MSI\\bot_key.key", 'r').readline())
+
     while (True):
         ten_minutes_ago = datetime.today() - timedelta(minutes=10)
         uploaded_videos = [i[0] for i in reader(open('uploaded_videos.csv', 'r'))]
-        video_search = [i['video_id'] for i in YOUTUBE.search(channel_id='UCyhEZKz-LOwgktptEOh6_Iw', published_after=ten_minutes_ago, order_by='date')]
+        video_search = [i['video_id'] for i in youtube.search(channel_id='UCyhEZKz-LOwgktptEOh6_Iw', published_after=ten_minutes_ago, order_by='date')]
         
         if not video_search:
             print("no new videos")
@@ -45,5 +67,19 @@ def main():
                     VideoHandler(vid)
             
 
+def testing():
+    # some = _youtube.search(channel_id='UCyhEZKz-LOwgktptEOh6_Iw', published_after=one_day_ago, order_by='date')
+    # print(some)
+    pass
+
+
 if __name__ == "__main__":
-    main()
+    # today = datetime.today()
+    # date = today.strftime('%m/%d/%Y')
+    # time = today.strftime('%I:%M %p')
+    # subject = 'Klavan Bot | INFO'
+    # mess = f'Dear Maker,\n\nOn {date} at {time}, I was started and have begun running my rounds.\n\nLove,\nBot'
+    # email(subject, mess)
+
+    # main()
+    testing()
